c4a99adb49c410f91adb55ab337c50fd8a6b8822
MattiasWiesner <mw@cloudcontrol.de>
2012-02-15 12:18:43 +0100

    add error

---
+++ error.php
@@ -0,0 +1,12 @@
+<?php
+
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    print_r(array($errno, $errstr, $errfile, $errline));
+}
+set_error_handler("myErrorHandler");
+
+function shutDownFunction() {
+    print_r(error_get_last());
+}
+register_shutdown_function('shutdownFunction');
+
