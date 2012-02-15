a1652346623b22eb58ca9100810824eed6c5e21d
MattiasWiesner <mw@cloudcontrol.de>
2012-02-15 11:46:32 +0100

    debug

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+require 'config.php';
+print_r($creds);
