ed125070c6130dbadbf63706700c7759dd947990
MattiasWiesner <mw@cloudcontrol.de>
2012-02-15 14:21:54 +0100

    debug

--- error.php
+++ error.php
@@ -1,30 +1,11 @@
 <?php
-$html = '
- <html>
-    <head></head>
-    <body>
-        <h1>Error</h1>
-        <pre style="disply:block;background-color:silver;border:1px groove;padding:5px;">
-            %s
-        </pre>
-    </body>
-</html>';
 
 function myErrorHandler($errno, $errstr, $errfile, $errline) {
-    global $html;
-    if ($errno) {
-        echo sprintf($html, print_r(array($errno, $errstr, $errfile, $errline), true));
-        die();
-    }
+    print_r(array($errno, $errstr, $errfile, $errline));
 }
 set_error_handler("myErrorHandler");
 
 function shutDownFunction() {
-    global $html;
-    $error = error_get_last();
-    if ($error) {
-        echo sprintf($html, print_r($error, true));
-        die();
-    }
+    print_r(error_get_last());
 }
 register_shutdown_function('shutdownFunction');
