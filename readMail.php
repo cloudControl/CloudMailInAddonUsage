ecea55eb27aa1333294434e080f3d5a7221b5456
MattiasWiesner <mw@cloudcontrol.de>
2012-02-15 15:33:45 +0100

    debug

--- readMail.php
+++ readMail.php
@@ -17,7 +17,7 @@ if (!$pdo) {
 }
 
 $select = <<<SQL
-SELECT `date`, `from`, `to`, `plain` FROM `mail`
+SELECT `date`, `from`, `to`, `post` FROM `mail`
 SQL;
 
 $result = array();
@@ -29,17 +29,17 @@ if ($selectStmt->execute()) {
 }
 $pdo->commit();
 
-#if(count($result) > 0){
-#    array_walk($result, function($mail){
-#        $mail->post = unserialize($mail->post);
-#    });
-#}
+if(count($result) > 0){
+    array_walk($result, function($mail){
+        $mail->post = unserialize($mail->post);
+    });
+}
 ?>
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head></head>
     <body>
-        <h1>Mails</h1>
+        <h1>Mail List</h1>
         <pre style="disply:block;background-color:silver;border:1px groove;padding:5px;">
             <?php
 print_r($result);
