d067618682136dd9e6815ad84499329b62b231b2
MattiasWiesner <mw@cloudcontrol.de>
2012-02-15 12:14:34 +0100

    debug

--- incomingMail.php
+++ incomingMail.php
@@ -1,6 +1,19 @@
 <?php
 require 'config.php';
 
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    print_r(array($errno, $errstr, $errfile, $errline));
+}
+
+set_error_handler("myErrorHandler");
+
+function shutDownFunction() {
+    print_r(error_get_last());
+}
+
+register_shutdown_function('shutdownFunction');
+
+
 $from = $_POST['from'];
 $to = $_POST['to'];
 $plain_text = $_POST['plain'];
--- readMail.php
+++ readMail.php
@@ -1,6 +1,20 @@
 <?php
 require 'config.php';
 
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    print_r(array($errno, $errstr, $errfile, $errline));
+}
+
+set_error_handler("myErrorHandler");
+
+function shutDownFunction() {
+    print_r(error_get_last());
+}
+
+register_shutdown_function('shutdownFunction');
+
+
+
 class MailDto {
     public $date;
     public $from;
